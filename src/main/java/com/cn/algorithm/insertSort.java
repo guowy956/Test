d5cfbb2a082e274a48bd961e50e05afa7e3ff314
wenyuan.guo@newtouch.cn <1234567890>
2017-08-15 11:04:47 +0800

    排序

--- src/main/java/com/cn/algorithm/insertSort.java
+++ src/main/java/com/cn/algorithm/insertSort.java
@@ -3,6 +3,10 @@ package com.cn.algorithm;
 /**
  * Created by newtouch on 2017/8/11.
  *  排序
+ *
+ *  基本思想：在要排序的一组数中，假设前面(n-1)[n>=2] 个数已经是排好顺序的，
+ *  现在要把第n 个数插到前面的有序数中，使得这 n个数也是排好顺序的。如此反复循环，
+ *  直到全部排好顺序。
  */
 public class insertSort {
 
--- src/main/java/com/cn/algorithm/shellSort.java
+++ src/main/java/com/cn/algorithm/shellSort.java
@@ -1,10 +1,12 @@
 package com.cn.algorithm;
 
-import org.springframework.scheduling.annotation.Scheduled;
-
 /**
  * Created by newtouch on 2017/8/11.
  * 希尔排序（最小增量排序）
+ *
+ * 算法先将要排序的一组数按某个增量 d（n/2,n为要排序数的个数）分成若干组，
+ * 每组中记录的下标相差 d.对每组中全部元素进行直接插入排序，然后再用一个较小的增量（d/2）对它进行分组，
+ * 在每组中再进行直接插入排序。当增量减到 1 时，进行直接插入排序后，排序完成。
  */
 public class shellSort {
 
@@ -12,11 +14,11 @@ public class shellSort {
 
     public static void main(String[] args) {
         int num[]={1,54,6,3,78,34,12,45,56,100};
-        double numLength = num.length;
+        double numLength = num.length; // 要排序数组的长度
         int temp = 0;
         while (true){
-            numLength = Math.ceil(numLength/2);
-            int nums = (int) numLength;
+            numLength = Math.ceil(numLength/2); //除法运算  -- 除二
+            int nums = (int) numLength;  // 转化为int
             for(int i= 0 ;i<nums;i++){
                  for(int j = i+nums ;j<num.length; j+=nums){
                      int k = j-nums;
